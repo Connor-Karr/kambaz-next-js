adc3cf828dfca1786db6fe9e0cb1b9e8b50af803
Connor-Karr <connorpkarr@gmail.com>
2026-02-08 20:37:19 -0500

    Finished all of 2.5

--- app/(kambaz)/account/profile/page.tsx
+++ app/(kambaz)/account/profile/page.tsx
@@ -1,19 +1,77 @@
 import Link from "next/link";
+import { FormControl, FormLabel, FormGroup } from "react-bootstrap";
+
 export default function Profile() {
   return (
-    <div id="wd-profile-screen">
+    <div id="wd-profile-screen" className="p-3">
       <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password"
-             className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
-      <select defaultValue="FACULTY" id="wd-role">
-        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link href="signin" > Sign out </Link>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-username">Username</FormLabel>
+        <FormControl
+          id="wd-username"
+          defaultValue="alice"
+          placeholder="username"
+          className="wd-username"
+        />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-password">Password</FormLabel>
+        <FormControl
+          id="wd-password"
+          defaultValue="123"
+          placeholder="password"
+          type="password"
+          className="wd-password"
+        />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-firstname">First Name</FormLabel>
+        <FormControl
+          id="wd-firstname"
+          defaultValue="Alice"
+          placeholder="First Name"
+        />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-lastname">Last Name</FormLabel>
+        <FormControl
+          id="wd-lastname"
+          defaultValue="Wonderland"
+          placeholder="Last Name"
+        />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-dob">Date of Birth</FormLabel>
+        <FormControl id="wd-dob" type="date" defaultValue="2000-01-01" />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-email">Email</FormLabel>
+        <FormControl
+          id="wd-email"
+          type="email"
+          defaultValue="alice@wonderland"
+        />
+      </FormGroup>
+
+      <FormGroup className="mb-3">
+        <FormLabel htmlFor="wd-role">Role</FormLabel>
+        <FormControl as="select" id="wd-role" defaultValue="FACULTY">
+          <option value="USER">User</option>
+          <option value="ADMIN">Admin</option>
+          <option value="FACULTY">Faculty</option>
+          <option value="STUDENT">Student</option>
+        </FormControl>
+      </FormGroup>
+
+      <Link href="/account/signin" className="btn btn-danger w-100">
+        Sign out
+      </Link>
     </div>
-);}
+  );
+}
--- app/(kambaz)/courses/[cid]/assignments/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/page.tsx
@@ -1,39 +1,104 @@
 import Link from "next/link";
+import { BsGripVertical, BsPlus } from "react-icons/bs";
+import { IoEllipsisVertical } from "react-icons/io5";
+import { FaRegEdit } from "react-icons/fa";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import AssignmentsControls from "./AssignmentsControls";
+import AssignmentControlButtons from "./AssignmentControlButtons";
+
 export default function Assignments() {
-    return (
-      <div id="wd-assignments">
-        <input placeholder="Search for Assignments"
-               id="wd-search-assignment" />
-        <button id="wd-add-assignment-group">+ Group</button>
-        <button id="wd-add-assignment">+ Assignment</button>
-        <h3 id="wd-assignments-title">
-          ASSIGNMENTS 40% of Total <button>+</button> </h3>
-        <ul id="wd-assignment-list">
-          <li className="wd-assignment-list-item">
-            <Link href="/courses/1234/assignments/123"
-               className="wd-assignment-link" >
-              A1 - ENV + HTML
-            </Link>
-            <p>Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am |</p>
-            <p><strong>Due</strong> May 13 at 11:59pm | 100 pts</p>
-          </li>
-          <li className="wd-assignment-list-item">
-            <Link href="/courses/1234/assignments/124"
-               className="wd-assignment-link" >
-              A2 - CSS + BOOTSTRAP
-            </Link>
-            <p>Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am |</p>
-            <p><strong>Due</strong> May 20 at 11:59pm | 100 pts</p>
-          </li>
-          <li className="wd-assignment-list-item">
-            <Link href="/courses/1234/assignments/125"
-               className="wd-assignment-link" >
-              A3 - JAVASCRIPT + REACT
-            </Link>
-            <p>Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am |</p>
-            <p><strong>Due</strong> May 27 at 11:59pm | 100 pts</p>
-          </li>
-        </ul>
-      </div>
+  return (
+    <div id="wd-assignments">
+      <AssignmentsControls />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <ListGroup className="rounded-0" id="wd-assignment-list">
+        <ListGroupItem className="wd-assignment-list-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center">
+            <BsGripVertical className="me-2 fs-3" />
+            <strong>ASSIGNMENTS</strong>
+            <span className="ms-2">40% of Total</span>
+            <div className="ms-auto">
+              <BsPlus className="fs-4" />
+              <IoEllipsisVertical className="fs-4" />
+            </div>
+          </div>
+
+          <ListGroup className="rounded-0">
+            <ListGroupItem className="wd-assignment-list-item p-3 ps-1 border border-0">
+              <div className="d-flex align-items-center">
+                <BsGripVertical className="me-2 fs-3" />
+                <FaRegEdit className="me-2 fs-5 text-success" />
+                <div className="flex-grow-1">
+                  <Link
+                    href="/courses/1234/assignments/123"
+                    className="wd-assignment-link fw-bold text-dark text-decoration-none"
+                  >
+                    A1 - ENV + HTML
+                  </Link>
+                  <div className="text-muted small">
+                    <span className="text-danger">Multiple Modules</span> |{" "}
+                    <strong>Not available until</strong> May 6 at 12:00am
+                  </div>
+                  <div className="text-muted small">
+                    <strong>Due</strong> May 13 at 11:59pm | 100 pts
+                  </div>
+                </div>
+                <AssignmentControlButtons />
+              </div>
+            </ListGroupItem>
+
+            <ListGroupItem className="wd-assignment-list-item p-3 ps-1 border border-0">
+              <div className="d-flex align-items-center">
+                <BsGripVertical className="me-2 fs-3" />
+                <FaRegEdit className="me-2 fs-5 text-success" />
+                <div className="flex-grow-1">
+                  <Link
+                    href="/courses/1234/assignments/124"
+                    className="wd-assignment-link fw-bold text-dark text-decoration-none"
+                  >
+                    A2 - CSS + BOOTSTRAP
+                  </Link>
+                  <div className="text-muted small">
+                    <span className="text-danger">Multiple Modules</span> |{" "}
+                    <strong>Not available until</strong> May 13 at 12:00am
+                  </div>
+                  <div className="text-muted small">
+                    <strong>Due</strong> May 20 at 11:59pm | 100 pts
+                  </div>
+                </div>
+                <AssignmentControlButtons />
+              </div>
+            </ListGroupItem>
+
+            <ListGroupItem className="wd-assignment-list-item p-3 ps-1 border border-0">
+              <div className="d-flex align-items-center">
+                <BsGripVertical className="me-2 fs-3" />
+                <FaRegEdit className="me-2 fs-5 text-success" />
+                <div className="flex-grow-1">
+                  <Link
+                    href="/courses/1234/assignments/125"
+                    className="wd-assignment-link fw-bold text-dark text-decoration-none"
+                  >
+                    A3 - JAVASCRIPT + REACT
+                  </Link>
+                  <div className="text-muted small">
+                    <span className="text-danger">Multiple Modules</span> |{" "}
+                    <strong>Not available until</strong> May 20 at 12:00am
+                  </div>
+                  <div className="text-muted small">
+                    <strong>Due</strong> May 27 at 11:59pm | 100 pts
+                  </div>
+                </div>
+                <AssignmentControlButtons />
+              </div>
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+      </ListGroup>
+    </div>
   );
 }
